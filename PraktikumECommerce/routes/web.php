52eb49121f05fdfd550e47f3ec358470ec747247
imaderiansuputra <ianputra935@gmail.com>
2022-03-01 00:19:20 +0800

    update theme

--- PraktikumECommerce/routes/web.php
+++ PraktikumECommerce/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', function () {
+    return view('layouts.main');
+});
+
+Route::get('shop', function () {
+    return view('homepage');
+});
+
+Route::get('item', function () {
+    return view('item');
+});
