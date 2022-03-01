c0fbea6422e5f4a3cb1da1f658496f0f16c8199e
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-03-02 03:08:00 +0800

    login page for users

---
+++ PraktikumECommerce/resources/views/pages/users/login/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.login.index')
+
+@section('title', 'Login User')
+
+@section('images')
+          <div class="img" style="background-image: url( {{asset('style/login/images/bg-1.jpg')}} );">
+          </div>
+@endsection
