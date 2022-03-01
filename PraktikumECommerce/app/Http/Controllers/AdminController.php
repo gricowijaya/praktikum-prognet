bd05a16b9564f4e9a75e6d60b3bae781196054f6
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-03-01 19:14:54 +0800

    add admin login controller in controller and models

---
+++ PraktikumECommerce/app/Http/Controllers/AdminController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admins;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class AdminController extends Controller
+{
+    public function showLogin()
+    {
+        return view('login-form-14.index');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'name' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::guard('admins')->attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('pegawai/dashboard');
+        }
+
+        return back()->withErrors([
+            'name' => 'username yang anda masukkan salah. Coba kembali!',
+        ]);
+    }
+  
+    public function logout(Request $request)
+    {
+        Auth::guard('admins')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('admins.showLogin');
+    }
+
+    public function dashboard()
+    {
+        // $ = Mahasiswa::where('status_mahasiswa', 1)->get()->count();
+        // $dosen = Dosen::where('status_dosen', 1)->get()->count();
+        // return view('pages.admins.dashboard.index', compact('mahasiswa', 'dosen'));
+        return view('pages.admins.dashboard.index');
+    }
+}
--- PraktikumECommerce/app/Models/Admins.php
+++ PraktikumECommerce/app/Models/Admins.php
@@ -6,11 +6,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Admin_Notification;
 use App\Models\Response;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admins extends Model
+class Admins extends Authenticatable
 {
     use HasFactory;
     protected $table = 'admins';
+
+    protected $guard = 'admins';
+
+    protected $fillable = [
+      'name', 'email', 'password',
+    ];
+
+    protected $hidden = [ 
+      'password', 'remember_token',
+    ];
 
     public function admin_notifications() {
       return $this->hasMany(Admin_Notification::class);
