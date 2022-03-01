6bbc5454a688f84c0a1d8319a5a0daabb455ede8
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-03-02 03:06:47 +0800

    update user and Admin controller

--- PraktikumECommerce/app/Http/Controllers/AdminController.php
+++ PraktikumECommerce/app/Http/Controllers/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
 {
     public function showLogin()
     {
-        return view('pages.admin.login.index');
+        return view('pages.admins.login.index');
     }
 
     public function login(Request $request)
--- PraktikumECommerce/app/Http/Controllers/UserController.php
+++ PraktikumECommerce/app/Http/Controllers/UserController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function showLogin()
+    {
+        return view('pages.users.login.index');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'name' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::guard('users')->attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('pegawai/dashboard');
+        }
+
+        return back()->withErrors([
+            'name' => 'username yang anda masukkan salah. Coba kembali!',
+        ]);
+    }
+  
+    public function logout(Request $request)
+    {
+        Auth::guard('users')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('users.showLogin');
+    }
+
+    public function dashboard()
+    {
+        return view('pages.users.dashboard.index');
+    }
 }
