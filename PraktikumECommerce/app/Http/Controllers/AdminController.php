64f1a70ffa1da3213e5b97000243aadeb2c611b4
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-03-01 21:22:11 +0800

    update showLogin function

--- PraktikumECommerce/app/Http/Controllers/AdminController.php
+++ PraktikumECommerce/app/Http/Controllers/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
 {
     public function showLogin()
     {
-        return view('login-form-14.index');
+        return view('pages.admin.login.index');
     }
 
     public function login(Request $request)
