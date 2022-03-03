d098e74668f9ede65ef5e7088bc796b066cf356f
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-03-03 16:54:36 +0800

    edit function showLogin() renamed to index() and add function for register_index() and register()

--- PraktikumECommerce/app/Http/Controllers/UserController.php
+++ PraktikumECommerce/app/Http/Controllers/UserController.php
@@ -5,41 +5,66 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Faker\Provider\Uuid;
 
 class UserController extends Controller
 {
-    public function showLogin()
+    public function index()
     {
         return view('pages.users.login.index');
+    }
+
+    public function register_index()
+    {
+        return view('pages.users.registration.index');
+    }
+
+    public function register(Request $request) { 
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        // if ($request->file('foto_mahasiswa')) {
+        //     $foto = $request->file('foto_mahasiswa');
+        //     $nameFile = date('ymdhis') . '_' . $request->nama . '.' . $foto->getClientOriginalExtension();
+        //     $foto->storeAs('foto_mahasiswa', $nameFile);
+        // }
+
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        return redirect()->route('users.login-index')->with('success', 'Users berhasil dibuat');
+        // return redirect()->route('pegawai.mahasiswa.index')->with('success', 'Data berhasil disimpan');
     }
 
     public function login(Request $request)
     {
         $credentials = $request->validate([
-            'name' => ['required'],
+            'email' => ['required'],
             'password' => ['required'],
         ]);
 
         if (Auth::guard('users')->attempt($credentials)) {
             $request->session()->regenerate();
-
-            return redirect()->intended('pegawai/dashboard');
+            return redirect()->intended('users/dashboard');
         }
 
         return back()->withErrors([
-            'name' => 'username yang anda masukkan salah. Coba kembali!',
+            'email' => 'Email yang anda masukkan salah. Coba input kembali!',
         ]);
     }
   
     public function logout(Request $request)
     {
         Auth::guard('users')->logout();
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
-
-        return redirect()->route('users.showLogin');
+        return redirect()->route('users.login-index');
     }
 
     public function dashboard()
