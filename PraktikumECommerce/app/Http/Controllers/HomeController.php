d81b3777a25f960a04921f07126f8eb68b1cd385
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-03-12 23:45:59 +0800

    view using auth.login

--- PraktikumECommerce/app/Http/Controllers/HomeController.php
+++ PraktikumECommerce/app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('pages.users.dashboard.index');
+        // return view('pages.users.dashboard.index');
+        return view('auth.login');
     }
 }
