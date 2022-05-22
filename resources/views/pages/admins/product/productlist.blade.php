6f12ba5d1a32a3c425459a0cb8868098b6e87f83
felix irwanto <felixirwanto25@gmail.com>
2022-05-22 18:48:31 +0800

    add delete button and message

--- resources/views/pages/admins/product/productlist.blade.php
+++ resources/views/pages/admins/product/productlist.blade.php
@@ -4,6 +4,22 @@
 @section('page2', 'Product List')
 
 @section('content')
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success alert-dismissible text-white" role="alert">
+            <span class="text-sm">{{ $message }}</span>
+            <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+    @if ($message = Session::get('error'))
+        <div class="alert alert-danger alert-dismissible text-white" role="alert">
+            <span class="text-sm">{{ $message }}</span>
+            <button type="button" class="btn-close text-lg py-3 opacity-10" data-bs-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
     <div class="main-panel">
         <div class="content-wrapper">
             <div class="grid-margin stretch-card">
@@ -53,7 +69,7 @@
                                                 <div class="d-flex align-items-center">
                                                     <a href="products/{{$product->id}}/show" class="m-1 btn bg-gradient-info"><i class="material-icons text-sm me-2">visibility</i>Detail</a>
                                                     <a href="products/{{$product->id}}/edit" class="m-1 btn bg-gradient-warning"><i class="material-icons text-sm me-2">edit</i>Edit</a>
-                                                    <a href="/admins/products/{{$product->id}}/delete" class="m-1 btn bg-gradient-danger" onclick="return confirx('Apa yakin ingin menghapus data ini?')"><i class="material-icons text-sm me-2">delete</i>Delete</a>
+                                                    <a href="/admins/products/{{$product->id}}/delete" class="m-1 btn bg-gradient-danger" onclick="return confirm('Apa yakin ingin menghapus data ini?')"><i class="material-icons text-sm me-2">delete</i>Delete</a>
                                                 </div>
                                             </td>                
                                         </tr>
