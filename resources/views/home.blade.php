224889d5fdc08f7a898da39a1ccb2a7c0092ffc2
felix irwanto <felixirwanto25@gmail.com>
2022-05-22 18:57:07 +0800

    modify home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,62 +1,22 @@
 @extends('layouts.user-layout.app')
+@section('title', 'Home')
 
 @section('content')
     <!-- Page Header Start -->
     <div class="container-fluid bg-secondary mb-5">
-        <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 300px">
+        <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 200px">
             <h1 class="font-weight-semi-bold text-uppercase mb-3">Welcome to Our Shop</h1>            
         </div>
     </div>
     <!-- Page Header End -->
 
     <div class="container-fluid pt-5">
-        <div class="row px-xl-5">
-            <!-- Shop Sidebar Start -->
-            <div class="col-lg-3 col-md-12">
-                <!-- Price Start -->
-                <div class="border-bottom mb-4 pb-4">
-                    <h5 class="font-weight-semi-bold mb-4">Filter by price</h5>
-                    <form>
-                        <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                            <input type="checkbox" class="custom-control-input" checked id="price-all">
-                            <label class="custom-control-label" for="price-all">All Price</label>
-                            <span class="badge border font-weight-normal">1000</span>
-                        </div>
-                        <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                            <input type="checkbox" class="custom-control-input" id="price-1">
-                            <label class="custom-control-label" for="price-1">$0 - $100</label>
-                            <span class="badge border font-weight-normal">150</span>
-                        </div>
-                        <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                            <input type="checkbox" class="custom-control-input" id="price-2">
-                            <label class="custom-control-label" for="price-2">$100 - $200</label>
-                            <span class="badge border font-weight-normal">295</span>
-                        </div>
-                        <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                            <input type="checkbox" class="custom-control-input" id="price-3">
-                            <label class="custom-control-label" for="price-3">$200 - $300</label>
-                            <span class="badge border font-weight-normal">246</span>
-                        </div>
-                        <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                            <input type="checkbox" class="custom-control-input" id="price-4">
-                            <label class="custom-control-label" for="price-4">$300 - $400</label>
-                            <span class="badge border font-weight-normal">145</span>
-                        </div>
-                        <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between">
-                            <input type="checkbox" class="custom-control-input" id="price-5">
-                            <label class="custom-control-label" for="price-5">$400 - $500</label>
-                            <span class="badge border font-weight-normal">168</span>
-                        </div>
-                    </form>
-                </div>
-                <!-- Price End -->              
-            </div>
-            <!-- Shop Sidebar End -->
-
+        
+            
 
             <!-- Shop Product Start -->
-            <div class="col-lg-9 col-md-12">
-                <div class="row pb-3">
+            
+                <div class="row px-xl-5 pb-3">
                     <div class="col-12 pb-1">
                         <div class="d-flex align-items-center justify-content-between mb-4">
                             <form action="">
@@ -64,7 +24,7 @@
                                     <input type="text" class="form-control" placeholder="Search by name">
                                     <div class="input-group-append">
                                         <span class="input-group-text bg-transparent text-primary">
-                                            <i class="fa fa-search"></i>
+                                            <i class="fa fa-search text-shopee"></i>
                                         </span>
                                     </div>
                                 </div>
@@ -87,7 +47,7 @@
                             <div class="card product-item border-0 mb-4">
                                 <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
                                     @foreach($products->product_images as $image)
-                                        <img class="img-fluid w-100" src="/uploads/product_images/{{$image->image_name}}" alt="">
+                                        <img class="img-fluid w-100" src="{{ asset($image->image_name) }}" alt="" style="width:300px; height:300px;">
                                         @break
                                     @endforeach
                                 </div>
@@ -98,11 +58,11 @@
                                     </div>
                                 </div>
                                 <div class="card-footer d-flex justify-content-between bg-light border">
-                                    <a href="/product/{{$products->id}}" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
+                                    <a href="/product/{{$products->id}}" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-shopee mr-1"></i>View Detail</a>
                                     @if (is_null(Auth::user()))
-                                        <a href="/login" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
+                                        <a href="/login" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-shopee mr-1"></i>Add To Cart</a>
                                     @else
-                                        <a href="/cart" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
+                                        <a href="/cart" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-shopee mr-1"></i>Add To Cart</a>
                                     @endif
                                 </div>
                             </div>
@@ -117,7 +77,7 @@
                                 <span class="sr-only">Previous</span>
                               </a>
                             </li>
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item active"><a class="page-link bg-shopee" href="#">1</a></li>
                             <li class="page-item"><a class="page-link" href="#">2</a></li>
                             <li class="page-item"><a class="page-link" href="#">3</a></li>
                             <li class="page-item">
@@ -130,9 +90,9 @@
                         </nav>
                     </div>
                 </div>
-            </div>
+            
             <!-- Shop Product End -->
-        </div>
+        
     </div>
                
 
