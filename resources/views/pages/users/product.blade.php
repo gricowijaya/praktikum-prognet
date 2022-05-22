32053e6673a2613d7f80c6ae39d387d8f0f8c7d4
felix irwanto <felixirwanto25@gmail.com>
2022-05-22 18:51:20 +0800

    image for product detail

--- resources/views/pages/users/product.blade.php
+++ resources/views/pages/users/product.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.user-layout.app')
+@section('title', 'Product Detail')
 
 @section('content')
     <div class="container-fluid py-5">
@@ -6,11 +7,10 @@
             <div class="col-lg-5 pb-5">
                 <div id="product-carousel" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner border">
-                        @foreach ($product->product_images as $image)
-                            <div class="carousel-item active">
-                                <img class="w-100 h-100" src="/uploads/product_images/{{ $image->image_name }}"
-                                    alt="Image">
-                            </div>
+                        @foreach($product->product_images as $image)
+                            <div class="carousel-item {{$loop->iteration == 1 ? 'active' : ''}}">                            
+                                <img class="h-50 w-50 center" src="{{ asset($image->image_name) }}" alt="Image" style="width: 350px; height: 350px;">                                                        
+                            </div>                        
                         @endforeach
                     </div>
                     <a class="carousel-control-prev" href="#product-carousel" data-slide="prev">
@@ -19,6 +19,7 @@
                     <a class="carousel-control-next" href="#product-carousel" data-slide="next">
                         <i class="fa fa-2x fa-angle-right text-dark"></i>
                     </a>
+                    
                 </div>
             </div>
 
@@ -26,57 +27,49 @@
                 <h3 class="font-weight-semi-bold">{{ $product->product_name }}</h3>
                 <div class="d-flex mb-3">
                     <div class="text-primary mr-2">
-                        <small class="fas fa-star"></small>
-                        <small class="fas fa-star"></small>
-                        <small class="fas fa-star"></small>
-                        <small class="fas fa-star-half-alt"></small>
-                        <small class="far fa-star"></small>
+                        <small class="fas fa-star text-shopee"></small>
+                        <small class="fas fa-star text-shopee"></small>
+                        <small class="fas fa-star text-shopee"></small>
+                        <small class="fas fa-star-half-alt text-shopee"></small>
+                        <small class="far fa-star text-shopee"></small>
                     </div>
                     <small class="pt-1">(50 Reviews)</small>
                 </div>
                 <h3 class="font-weight-semi-bold mb-4">@currency($product->price)</h3>
                 <p class="mb-4">{{ $product->description }}</p>
-                <div class="d-flex align-items-center mb-4 pt-2">
+                                <div class="d-flex align-items-center mb-4 pt-2">
                     <div class="input-group quantity mr-3" style="width: 130px;">
                         <div class="input-group-btn">
-                            <button class="btn btn-primary btn-minus">
-                                <i class="fa fa-minus"></i>
+                            <button class="btn btn-primary btn-minus bg-shopee" >
+                            <i class="fa fa-minus"></i>
                             </button>
                         </div>
-                        <input type="text" name="qty" id="qty" class="form-control text-center" value="1">
+                        <input type="text" class="form-control bg-secondary text-center" value="1">
                         <div class="input-group-btn">
-                            <button class="btn btn-primary btn-plus">
+                            <button class="btn btn-primary btn-plus bg-shopee">
                                 <i class="fa fa-plus"></i>
                             </button>
                         </div>
                     </div>
                     @if (is_null(Auth::user()))
-                        <a href="/login" class="btn btn-primary px-3"><i class="fa fa-shopping-cart mr-1"></i> Add To
-                            Cart</a>
+                        <a href="/login"class="btn btn-primary px-3 bg-shopee"><i class="fa fa-shopping-cart mr-1"></i> Add To Cart</a>
                     @else
-                        <form method="POST" id="formStoreCart" action="/cart">
-                            @csrf
-                            {{ method_field('post') }}
-                            <input type="hidden" name="product_id" id="product_id" value="{{ $product->id }}">
-                            <input type="hidden" name="qty_final" id="qty_final" value="1">
-                            <button type="submit" class="btn btn-primary px-3"><i class="fa fa-shopping-cart mr-1"></i> Add
-                                To Cart</button>
-                        </form>
-                    @endif
-                </div>
+                        <a href="/cart"class="btn btn-primary px-3 bg-shopee"><i class="fa fa-shopping-cart mr-1"></i> Add To Cart</a>
+                    @endif                    
+                </div>                
             </div>
         </div>
         <div class="row px-xl-5">
             <div class="col">
                 <div class="nav nav-tabs justify-content-center border-secondary mb-4">
-                    <a class="nav-item nav-link active" data-toggle="tab" href="#tab-pane-1">Description</a>
+                    <a class="nav-item nav-link active" data-toggle="tab" href="#tab-pane-1">Description</a>                    
                     <a class="nav-item nav-link" data-toggle="tab" href="#tab-pane-3">Reviews (0)</a>
                 </div>
                 <div class="tab-content">
                     <div class="tab-pane fade show active" id="tab-pane-1">
                         <h4 class="mb-3">Product Description</h4>
-                        <p>{{ $product->description }}</p>
-                    </div>
+                        <p>{{ $product->description }}</p>                        
+                    </div>                   
                     <div class="tab-pane fade" id="tab-pane-3">
                         <div class="row">
                             <div class="col-md-6">
@@ -92,8 +85,7 @@
                                             <i class="fas fa-star-half-alt"></i>
                                             <i class="far fa-star"></i>
                                         </div>
-                                        <p>Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no
-                                            at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.</p>
+                                        <p>Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.</p>
                                     </div>
                                 </div>
                             </div>
@@ -124,7 +116,7 @@
                                         <input type="email" class="form-control" id="email">
                                     </div>
                                     <div class="form-group mb-0">
-                                        <input type="submit" value="Leave Your Review" class="btn btn-primary px-3">
+                                        <input type="submit" value="Leave Your Review" class="btn btn-primary bg-shopee px-3 text-white">
                                     </div>
                                 </form>
                             </div>
@@ -134,4 +126,8 @@
             </div>
         </div>
     </div>
+
+
+
+    
 @endsection
