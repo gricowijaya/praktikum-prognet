b469e7347d28639e82d8bfb8ea01ab9806f44254
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-05-22 18:25:38 +0800

    update model carts

--- app/Models/carts.php
+++ app/Models/carts.php
@@ -9,18 +9,15 @@ use App\Models\User;
 
 class carts extends Model
 {
-  use HasFactory;
+    use HasFactory;
 
-  protected $table = 'carts';
-  protected $guarded = 'id';
+    protected $table = 'carts';
 
-  public function product()
-  {
-    return $this->belongsTo(Product::class, 'product_id', 'id');
-  }
+    public function product() { 
+      return $this->belongsTo(Product::class);
+    }
 
-  public function user()
-  {
-    return $this->belongsTo(User::class, 'user_id', 'id');
-  }
-}
+    public function user() { 
+      return $this->belongsTo(User::class);
+    }
+} 
