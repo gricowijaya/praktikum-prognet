61786cbc8f137d3990c7be69c96572766e99092e
imaderiansuputra <ianputra935@gmail.com>
2022-05-29 22:21:17 +0800

    update view home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,23 +16,13 @@
             <!-- Shop Product Start -->
             <div class="col-lg-9 col-md-12">
                 <div class="row pb-3">
-                    <div class="col-12 pb-1">
-                        <div class="d-flex align-items-center justify-content-between mb-4">
-                            <div class="dropdown ml-4">
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="triggerId">
-                                    <a class="dropdown-item" href="#">Latest</a>
-                                    <a class="dropdown-item" href="#">Popularity</a>
-                                    <a class="dropdown-item" href="#">Best Rating</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
                     @foreach ($product as $products)
-                    <div class="col-lg-6 col-md-6 col-sm-12 pb-1">
+                    <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
                             <div class="card product-item border-0 mb-4">
                                 <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
                                     @foreach($products->product_images as $image)
-                                        <img class="img-fluid w-100" src="{{asset($image->image_name)}}" alt="">
+                                        <img class="" src="{{asset($image->image_name)}}" alt="" style="width:300px; height:300px;:">
                                         @break
                                     @endforeach
                                 </div>
@@ -53,27 +43,7 @@
                             </div>
                         </div>            
                     @endforeach                    
-                    <div class="col-12 pb-1">
-                        <nav aria-label="Page navigation">
-                          <ul class="pagination justify-content-center mb-3">
-                            <li class="page-item disabled">
-                              <a class="page-link" href="#" aria-label="Previous">
-                                <span aria-hidden="true">&laquo;</span>
-                                <span class="sr-only">Previous</span>
-                              </a>
-                            </li>
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                              <a class="page-link" href="#" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                                <span class="sr-only">Next</span>
-                              </a>
-                            </li>
-                          </ul>
-                        </nav>
-                    </div>
+                    
                 </div>
             </div>
 
